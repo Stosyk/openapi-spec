61c41bc0c73f46a8c7430bc7e960dcb76a6c580e
maksym.koshtenko <maxim.koshtenko@auto1.com>
2017-04-16 23:25:43 +0300

    Update tests

--- scripts/test.js
+++ scripts/test.js
@@ -1,21 +1,19 @@
 #!/usr/bin/env node
 'use strict';
-var Path = require('path');
-
+var path = require('path');
 require('shelljs/global');
-set('-e');
 
 mkdir('-p', 'spec')
-cp('-R', 'resources/public/*', 'spec/');
+cp('-R', path.join('resources', 'public', '*'), 'spec/');
 exec('npm run swagger validate');
 rm('-rf', 'spec')
 
 mkdir('-p', 'spec')
-cp('-R', 'resources/manage/*', 'spec/');
+cp('-R', path.join('resources', 'manage', '*'), 'spec/');
 exec('npm run swagger validate');
 rm('-rf', 'spec')
 
 mkdir('-p', 'spec')
-cp('-R', 'resources/admin/*', 'spec/');
+cp('-R', path.join('resources', 'admin', '*'), 'spec/');
 exec('npm run swagger validate');
 rm('-rf', 'spec')
